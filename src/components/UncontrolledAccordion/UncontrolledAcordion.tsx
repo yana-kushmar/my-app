4d353d221af2fcc4b58908a4d60ca109b376fa74
yana-kushmar <yana.puzevich@gmail.com>
2023-04-11 10:57:26 -0400

    UseReducer

--- src/components/UncontrolledAccordion/UncontrolledAcordion.tsx
+++ src/components/UncontrolledAccordion/UncontrolledAcordion.tsx
@@ -1,36 +1,69 @@
-import React, {useState} from "react";
+import React, {useReducer, useState} from "react";
 
 
 type UnAccordionPropsType = {
     titleValue: string;
     collapsed?: boolean;
 }
-function UncontrolledAccordion (props: UnAccordionPropsType) {
 
-    const [collapsed, setCollapsed]= useState(false);
+
+type ActionType = {
+    type: string
+}
+
+const TOGGLE_CONSTANT = "TOGGLE-COLLAPSED"
+
+type StateType = {
+    collapsed: boolean
+}
+const reducer = (state: StateType, action: ActionType):StateType => {
+    switch (action.type) {
+        case TOGGLE_CONSTANT:
+           return {
+                ...state,
+                collapsed: !state.collapsed
+            }
+        default:
+            throw new Error("Bad action type")
+    }
+    return state
+}
+
+function UncontrolledAccordion(props: UnAccordionPropsType) {
+
+    // const [collapsed, setCollapsed]= useState(false);
+    let [state, dispatch] = useReducer(reducer, {collapsed: false});
+
 
     return <div>
+        {/*<UnAccordionTitle title={props.titleValue}*/}
+        {/*                  onClick={() => {*/}
+        {/*                      setCollapsed(!collapsed)*/}
+        {/*                  }}/>*/}
         <UnAccordionTitle title={props.titleValue}
-                        onClick={() => {setCollapsed(!collapsed)}}/>
-        {!collapsed && <UnAccordionBody />}
+                          onClick={() => {dispatch({type: TOGGLE_CONSTANT})
+                          }}/>
+        {!state.collapsed && <UnAccordionBody/>}
 
 
     </div>
 }
 
 type UnAccordionTitlePropsType = {
-    title : string;
+    title: string;
     onClick: () => void
 }
 
 function UnAccordionTitle(props: UnAccordionTitlePropsType) {
-    return(
-        <h3 onClick={() => {props.onClick()}}>--- {props.title} ---</h3>
+    return (
+        <h3 onClick={() => {
+            props.onClick()
+        }}>--- {props.title} ---</h3>
     )
 }
 
 
-function UnAccordionBody(){
+function UnAccordionBody() {
     return (
         <ul>
             <li>1</li>
@@ -41,4 +74,5 @@ function UnAccordionBody(){
     )
 
 }
+
 export default UncontrolledAccordion;
